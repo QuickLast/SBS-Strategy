d644dcab56e7312090712f7755b0a78ded057ba8
202113 <202113@1220-001-22>
2024-02-16 11:09:32 +0300

    Started working on weapons

---
+++ SBS-Game/Model/Dagger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SBS_Game.Model
+{
+    public class Dagger
+    {
+        public bool isTwoHanded = false;
+        public bool canBeUsedWithShield = true;
+        public bool canBeDual = true;
+        public string rarity = "common";
+
+        public int pDamageRatio = 4;
+        public int manaRatio = 20;
+        public int intelligenceRatio = 20;
+
+        public static int crtChanse = 0;
+        public static int crtDamage = 0;
+        public int magicCrit = Convert.ToInt32(crtChanse * 0.05 + crtDamage * 3);
+    }
+}
---
+++ SBS-Game/Model/Weapon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SBS_Game.Model
+{
+    public class Weapon
+    {
+        bool isTwoHanded = false;
+        bool canBeUsedWithShield = false;
+        bool canBeDual = false;
+        string rarity = "common";
+    }
+}
